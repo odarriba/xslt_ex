ff4652098b7d16315767cd6a2105b42cb60725fe
Óscar de Arriba <odarriba@gmail.com>
2018-02-26 17:49:24 +0100

    From elixir 1.4

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule XsltEx.MixProject do
     [
       app: :xslt_ex,
       version: "0.1.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.4",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       compilers: [:xslt_ex] ++ Mix.compilers()
